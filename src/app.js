f0b1339552eb522a75c27fe2cb03868248908d6a
Emiliano <emi.perez997@gmail.com>
2023-05-20 11:21:48 -0300

    [ADD] Views

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
-
+import __dirname from "./dirname.js";
 import usersRouter from "./routes/users.router.js";
 import petsRouter from "./routes/pets.router.js";
 import adoptionsRouter from "./routes/adoption.router.js";
 import sessionsRouter from "./routes/sessions.router.js";
+import viewsRouter from "./routes/views.router.js";
+import handlebars from "express-handlebars";
+import Handlebars from "handlebars";
+import { allowInsecurePrototypeAccess } from "@handlebars/allow-prototype-access";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -14,6 +18,23 @@ const connection = mongoose.connect("mongodb://localhost:27017/adoptme");
 app.use(express.json());
 app.use(cookieParser());
 
+// Handlebars
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+    defaultLayout: "main",
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+  })
+);
+
+// Configuracion de Express
+app.set("view engine", "hbs");
+app.set("views", `${__dirname}/views`);
+
+app.use(express.static(`${__dirname}/public`));
+
+app.use("/", viewsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/pets", petsRouter);
 app.use("/api/adoptions", adoptionsRouter);
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,2 +1,24 @@
 import { Router } from "express";
+import { usersService, petsService } from "../services/index.js";
 
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.render("home");
+});
+
+router.get("/users", async (req, res) => {
+  const users = await usersService.getAll();
+  res.render("users", {
+    users,
+  });
+});
+
+router.get("/pets", async (req, res) => {
+  const pets = await petsService.getAll();
+  res.render("pets", {
+    pets,
+  });
+});
+
+export default router;
